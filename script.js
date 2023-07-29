d1ec1e7f1bfb9194e13eeffa48df2a264d5db7e6
Bernardo Martelli <bernardo.martelli@gmail.com>
2023-07-29 08:05:15 +0200

    update also vector3 values

--- script.js
+++ script.js
@@ -1,72 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 
-const startValues = {
-  ObjectRotation: -25.43482,
-  CX: 4.078432,
-  CY: 16,
-  CZ: 2.196079,
-  ang: -0.2941177,
-  scale: 3.098039,
-  maxIterations: 13,
-  Bailout: 4,
-  PhongPower: 1,
-  ShadowDensity: 0.1372549,
-  AmbientOcclusionColor: "0, 0, 0.16875",
-  AmbientOcclusionDensity: 0.58,
-  AmbientOcclusionEmphasis: 0.85,
-  ColorSpread: 0.7156863,
-  RimLightFactor: 0,
-  Specularity: 0.66,
-  Shinyness: 15,
-  EpsilonScale: 1,
-  RaytracingStepLimit: 2000,
-  BoundingSphereRadius: 16,
-  CameraPosition: "-4, -2.173913, 0.7826087",
-  CameraRotation: "121.3043, 117.3913, 86.08696",
-  CameraZoom: 0,
-  BackgroundColor: "0, 0, 0, 0",
-  BackgroundImage: "<null>",
-  AmbientColor: "0, 0, 0, 0",
-  DiffuseColor: "0.83125, 0.85, 0.6625",
-  LightPosition: "-8.652174, -25.69565, -13.54348",
-  LightColor: "0, 1, 1",
-  Exposure: 0.4705882,
-};
-
-const endValues = {
-  ObjectRotation: 50.23913,
-  CX: 16,
-  CY: 32,
-  CZ: 5.196079,
-  ang: 0.2941177,
-  scale: 5.098039,
-  maxIterations: 50,
-  Bailout: 10,
-  PhongPower: 10,
-  ShadowDensity: 0.3372549,
-  AmbientOcclusionColor: "0, 0, 0.36875",
-  AmbientOcclusionDensity: 1,
-  AmbientOcclusionEmphasis: 1,
-  ColorSpread: 1,
-  RimLightFactor: 0.5,
-  Specularity: 0.8,
-  Shinyness: 30,
-  EpsilonScale: 5,
-  RaytracingStepLimit: 10000,
-  BoundingSphereRadius: 32,
-  CameraPosition: "-8, -4.173913, 1.7826087",
-  CameraRotation: "241.3043, 237.3913, 206.08696",
-  CameraZoom: 50,
-  BackgroundColor: "1, 1, 1, 1",
-  BackgroundImage: "<image>",
-  AmbientColor: "1, 1, 1, 1",
-  DiffuseColor: "1.83125, 1.85, 1.6625",
-  LightPosition: "-16.652174, -50.69565, -30.54348",
-  LightColor: "1, 2, 2",
-  Exposure: 1.4705882,
-};
-
 const numberOfFiles = 100;
 const stepSize = 1 / (numberOfFiles - 1);
 
@@ -77,33 +11,71 @@ if (!fs.existsSync(outputDir)) {
   fs.mkdirSync(outputDir);
 }
 
-// Helper function to interpolate between start and end values
-function interpolateValues(startValue, endValue, t) {
-  if (typeof startValue === "number" && typeof endValue === "number") {
-    return startValue + t * (endValue - startValue);
-  } else if (typeof startValue === "string" && typeof endValue === "string") {
-    const startComponents = startValue.split(", ");
-    const endComponents = endValue.split(", ");
-    const interpolatedComponents = startComponents.map((start, index) => {
-      const end = parseFloat(endComponents[index]);
-      return (parseFloat(start) + t * (end - parseFloat(start))).toFixed(6);
-    });
-    return interpolatedComponents.join(", ");
-  } else {
-    throw new Error("Invalid data type for interpolation.");
+// Helper function to interpolate between start and end values for variables with multiple numbers
+function interpolateMultiValues(startValue, endValue, t) {
+  const startValuesArr = startValue.split(", ");
+  const endValuesArr = endValue.split(", ");
+
+  if (startValuesArr.length !== endValuesArr.length) {
+    throw new Error(
+      "Number of values in start and end values must be the same for multi-value variables."
+    );
   }
+
+  const interpolatedValuesArr = startValuesArr.map((start, index) => {
+    const end = parseFloat(endValuesArr[index]);
+    return (parseFloat(start) + t * (end - parseFloat(start))).toFixed(6);
+  });
+
+  return interpolatedValuesArr.join(", ");
 }
+
+// Read startValues and endValues from file1.txt and file2.txt respectively
+const file1Content = fs.readFileSync("keyframe1.txt", "utf8");
+const file2Content = fs.readFileSync("keyframe2.txt", "utf8");
+
+console.log("File 1 Content:");
+console.log(file1Content);
+
+console.log("\nFile 2 Content:");
+console.log(file2Content);
+
+const startValues = {};
+const endValues = {};
+
+file1Content.split("\n").forEach((line) => {
+  const [key, value] = line.trim().split(" = ");
+  startValues[key] = value;
+});
+
+file2Content.split("\n").forEach((line) => {
+  const [key, value] = line.trim().split(" = ");
+  endValues[key] = value;
+});
+
+console.log("\nStart Values:");
+console.log(startValues);
+
+console.log("\nEnd Values:");
+console.log(endValues);
 
 for (let i = 0; i < numberOfFiles; i++) {
   const t = i * stepSize;
   const interpolatedValues = {};
 
   for (const key of Object.keys(startValues)) {
-    interpolatedValues[key] = interpolateValues(
-      startValues[key],
-      endValues[key],
-      t
-    );
+    const startValue = startValues[key];
+    const endValue = endValues[key];
+
+    if (startValue.includes(",")) {
+      // For variables with multiple values, call the helper function to interpolate each value
+      interpolatedValues[key] = interpolateMultiValues(startValue, endValue, t);
+    } else {
+      // For single-value variables, use regular interpolation
+      interpolatedValues[key] =
+        parseFloat(startValue) +
+        t * (parseFloat(endValue) - parseFloat(startValue));
+    }
   }
 
   const fileName = `file_${i + 1}.txt`;
@@ -120,3 +92,5 @@ for (let i = 0; i < numberOfFiles; i++) {
     }
   });
 }
+
+console.log("Finished creating files.");
